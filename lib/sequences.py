38336811c5f1266eba7a43f3e86682ef5f0a8493
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-05-25 23:43:54 +0300

    Commit changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length <= 0:
+        print([])
+        return
+    
+    fib_seq = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_seq.append(a)
+        a, b = b, a + b
+
+    print(fib_seq)
     pass
